2066ab8a1c9d7b2e18c9ed8c43c459bec11cc637
zg163 <b.zougang@gmail.com>
2018-12-01 11:08:09 +0800

    增加读取txt文件，目录，通配符处理API实现

--- spark/python/io/textFile.py
+++ spark/python/io/textFile.py
@@ -7,18 +7,73 @@ import os
 
 from pyspark import SparkConf, SparkContext
 
-conf = (SparkConf()
-    .setMaster('local')
-    .setAppName("textFile input")
-    .set("spark.executor.memory", "2g")
-)
-sc = SparkContext(conf=conf)
 
-file_path = os.path.abspath(os.path.join(
-    os.path.dirname(__file__),
-    'data',
-    'README.md'
-))
+def read_file():
+    file_path = os.path.abspath(
+        os.path.join(
+            os.path.dirname(
+                os.path.dirname(__file__),
+            ),
+        'data',
+        'README.md'
+        )
+    )
+    inputRDD = sc.textFile("file://%s" % file_path)
+    print inputRDD.collect()
+    return inputRDD
 
-inputRDD = sc.textFile("file://%s" % file_path)
-print inputRDD.collect()
+
+def read_directory():
+    dir_path = os.path.abspath(
+        os.path.join(
+            os.path.dirname(
+                os.path.dirname(__file__)
+            ),
+            'data',
+            'fake_logs',
+        )
+    )
+    inputRDD = sc.textFile("file://%s" % dir_path)
+    print inputRDD.count()
+    return inputRDD
+
+
+def read_with_wildcard():
+    log_files = os.path.abspath(
+        os.path.join(
+            os.path.dirname(
+                os.path.dirname(__file__)
+            ),
+            'data',
+            'fake_logs',
+            '*.log'
+        )
+    )
+    inputRDD = sc.textFile("file://%s" % log_files)
+    print inputRDD.collect()
+    return inputRDD
+
+
+
+def output(result):
+    outputpath = os.path.abspath(
+        os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            'data',
+            'output',
+        )
+    )
+    result.saveAsTextFile(outputpath)
+
+
+if __name__ == "__main__":
+    conf = (SparkConf()
+        .setMaster('local')
+        .setAppName("textFile input")
+        .set("spark.executor.memory", "2g")
+    )
+    sc = SparkContext(conf=conf)
+    #inputRDD = read_file()
+    #inputRDD = read_directory()
+    inputRDD = read_with_wildcard()
+    output(inputRDD)
--- spark/python/io/wholeTextFiles.py
+++ spark/python/io/wholeTextFiles.py
@@ -8,18 +8,72 @@ import os
 
 from pyspark import SparkConf, SparkContext
 
-conf = (SparkConf()
-    .setMaster('local')
-    .setAppName("wholeTextFiles input")
-    .set("spark.executor.memory", "2g")
-)
-sc = SparkContext(conf=conf)
+def read_file():
+    file_path = os.path.abspath(
+        os.path.join(
+            os.path.dirname(
+                os.path.dirname(__file__),
+            ),
+        'data',
+        'README.md'
+        )
+    )
+    inputRDD = sc.wholeTextFiles("file://%s" % file_path)
+    print inputRDD.collect()
+    return inputRDD
 
-file_path = os.path.abspath(os.path.join(
-    os.path.dirname(__file__),
-    'data',
-    'README.md'
-))
 
-inputRDD = sc.wholeTextFiles("file://%s" % file_path)
-print inputRDD.collect()
+def read_directory():
+    dir_path = os.path.abspath(
+        os.path.join(
+            os.path.dirname(
+                os.path.dirname(__file__)
+            ),
+            'data',
+            'fake_logs',
+        )
+    )
+    inputRDD = sc.wholeTextFiles("file://%s" % dir_path)
+    print inputRDD.count()
+    return inputRDD
+
+
+def read_with_wildcard():
+    log_files = os.path.abspath(
+        os.path.join(
+            os.path.dirname(
+                os.path.dirname(__file__)
+            ),
+            'data',
+            'fake_logs',
+            '*.log'
+        )
+    )
+    inputRDD = sc.wholeTextFiles("file://%s" % log_files)
+    print inputRDD.collect()
+    return inputRDD
+
+
+
+def output(result):
+    outputpath = os.path.abspath(
+        os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            'data',
+            'output',
+        )
+    )
+    result.saveAsTextFile(outputpath)
+
+
+if __name__ == "__main__":
+    conf = (SparkConf()
+        .setMaster('local')
+        .setAppName("wholeTextFiles input")
+        .set("spark.executor.memory", "2g")
+    )
+    sc = SparkContext(conf=conf)
+    #inputRDD = read_file()
+    #inputRDD = read_directory()
+    inputRDD = read_with_wildcard()
+    output(inputRDD)
